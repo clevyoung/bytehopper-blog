5f2466d007e79240de91570e659f8547cd3cd4f5
clevyoung <clevyoung@gmail.com>
2023-11-14 22:31:25 +0900

    config: Configure svg

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@ const { withContentlayer } = require('next-contentlayer');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
 };
 
 module.exports = withContentlayer({ ...nextConfig });
