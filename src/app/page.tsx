42fcaf5683ff2ee37978cb44516973b6789f1270
clevyoung <clevyoung@gmail.com>
2023-11-17 22:44:06 +0900

    Add Hero component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,25 @@
+import NextImage from 'next/image';
+
 export default function Home() {
-  return <main className='flex min-h-screen flex-col items-center justify-between p-24'></main>;
+  return (
+    <main className='min-h-screen'>
+      <div className='flex h-[20rem] items-center bg-hero bg-cover bg-center'>
+        <div className='w-section flex w-full items-center gap-x-6'>
+          <div>
+            <NextImage
+              src='/static/images/hero-rabbit.svg'
+              alt='hero rabbit'
+              width={110}
+              height={125}
+            />
+          </div>
+          <div>
+            <h1 className='text-5xl font-bold text-white'>ByteHopper Blog</h1>
+            <p className='mt-4 text-white'>개발 블로그입니다.</p>
+          </div>
+        </div>
+      </div>
+      <div className='w-section mx-auto'></div>
+    </main>
+  );
 }
